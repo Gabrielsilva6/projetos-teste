607a8839a2a41278c877f66586451e34b89dc0dd
GABRIEL KAIKY <147208997+Gabrielsilva6@users.noreply.github.com>
2024-01-06 15:35:00 -0300

    Menu-Opções

---
+++ menu-opcoes/script/script.js
@@ -0,0 +1,45 @@
+var txtop = document.getElementById('opcao')
+var esc = document.getElementById('res')
+
+function enviar() {
+    var op = Number.parseInt(txtop.value)
+    
+    switch (op) {
+        case 1:
+            var txtn1 = window.prompt("Digite um valor: ")
+            var txtn2 = window.prompt("Digite outro valor: ")
+            var n1 = Number(txtn1)
+            var n2 = Number(txtn2)
+            var s = n1 + n2
+            esc.innerHTML = `A soma entre ${n1} + ${n2} é igual a : <strong>${s}</strong>`
+        break
+
+        case 2:
+            var txtn1 = window.prompt("Digite um valor: ")
+            var txtn2 = window.prompt("Digite outro valor: ")
+            var n1 = Number(txtn1)
+            var n2 = Number(txtn2)
+            var s = n1 * n2
+            esc.innerHTML = `A soma entre ${n1} x ${n2} é igual a : <strong>${s}</strong>`
+        break
+
+        case 3:
+            var txtn1 = window.prompt("Digite um número: ")
+            var n1 = Number(txtn1)
+            if (n1 % 2 == 0) {
+                esc.innerHTML = `O número ${n1} é PAR`
+            } else {
+                esc.innerHTML = `O número ${n1} é ÍMPAR`
+            }
+        break
+
+        case 4:
+            var txtat = window.prompt("Digite o ano atual: ")
+            var txtan = window.prompt("Digite o ano que você nasceu: ")
+            var at = Number(txtat)
+            var an = Number(txtan)
+            var idade = at - an
+            esc.innerHTML = `Em ${at} você terá: <strong>${idade} anos</strong>`
+        break
+    }
+}
