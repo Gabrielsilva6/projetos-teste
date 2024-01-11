7cf57c28d84487085c90488feddc135da35602ad
GABRIEL KAIKY <147208997+Gabrielsilva6@users.noreply.github.com>
2024-01-11 15:57:28 -0300

    .

--- calculadora-v1/script/script.js
+++ calculadora-v1/script/script.js
@@ -1,4 +1,14 @@
 
 function insert(num){
-    document.getElementById('resultado').innerHTML = num
+    document.getElementById('resultado').innerHTML += num
+}
+
+function clean() {
+    var esc = document.getElementById('resultado')
+    esc.innerHTML = ''
+}
+
+function result() {
+    var esc = document.getElementById('resultado')
+    
 }
